778746233af19e61f3b955c9c65cc53bf7da5874
Vu Cao <cdhvu@yahoo.com>
2019-01-14 21:22:16 -0800

    Add Header html

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class="inner">
+      <a href="#" class="image avatar">
+        <img src="" alt="" />
+      </a>
+      <h1>
+        <strong>I am Vu Cao</strong>, a dedicated
+        <br />
+        and passionate
+        <br />
+        web developer.
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
