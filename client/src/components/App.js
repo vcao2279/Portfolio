e9599fc0e8174591262796f6476f4f57e36d77c5
Vu Cao <cdhvu@yahoo.com>
2019-01-14 21:31:55 -0800

    Add About Me html

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,6 +8,27 @@ class App extends Component {
         <header className="App-header">
           <Header />
         </header>
+
+        <div id="main">
+          <section id="one">
+            <header class="major">
+              <h2>About Me</h2>
+            </header>
+            <p>
+              Hello, I'm Vu, a software developer from Orange County,
+              California. I love tackling challenges and finding solutions to
+              complex problems. MY passion is to create apps that make positive
+              impacts on others.
+            </p>
+            <ul class="actions">
+              <li>
+                <a href="#" class="button">
+                  My Resume
+                </a>
+              </li>
+            </ul>
+          </section>
+        </div>
       </div>
     );
   }
